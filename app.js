0f0d40e8e83912afb43777b5dc1da4d0ae4b51ff
Prabej Hussain <contact.prabej@gmail.com>
2024-01-14 08:49:49 +0545

    initial

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+const bodyParser = require('body-parser');
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+
+let city = "Birgunj";
+
+app.post("/",(req,res)=>{
+    city = req.body.cityName;
+    console.log(city);
+    res.redirect("/");
+});
+
+app.get("/",(req,res)=>{
+    
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=63b179f7aed0bc467436b03999f61ff4&units=metric";
+    const request = https.get(url,(response)=>{
+        response.on("data",(data)=>{
+            const waetherData = JSON.parse(data);
+            res.render("index",{temperature: waetherData.main.temp,Dis:waetherData.weather[0].description,imgCode:'https://openweathermap.org/img/wn/' + waetherData.weather[0].icon +'@2x.png',city:city});
+
+        });
+    })
+
+});
+
+
+
+app.listen(3000,(req,res)=>{
+    console.log("Server is running at port 3000");
+});
